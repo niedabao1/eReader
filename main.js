64ac548c6b96024fd2a3647f511d94de7ffcf896
niedabao1 <nieyuxiao42@gmail.com>
2021-10-14 16:47:30 +0800

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({ //创建一个app
+    data() {
+        return {
+            product: 'Socks',
+            // Solution
+            description: 'A warm fuzzy pair of socks.' 
+            // Solution
+        }
+    }
+})
